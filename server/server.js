971d32ddb9bd5668d65720880b18a2fa26b5ce4a
инка2511 <inzuakylbekova@gmail.com>
2025-06-25 17:51:13 +0500

    112

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ import path from 'path';
 import fs from 'fs';
 import axios from 'axios';
 import nodemailer from 'nodemailer';
+import path from 'path';
 
 // Load environment variables
 dotenv.config();
@@ -17,9 +18,22 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true,
+}));
 app.use(express.json());
+const allowedOrigins = [
+  'https://zhezu.onrender.com',
+  'http://localhost:5173',
+  'https://zhezu-front.onrender.com/'
+];
 
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 // File upload configuration
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
